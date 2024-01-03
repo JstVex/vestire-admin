aed63acd80c528a00b8f1b1f29ea2791a06780e0
JstVex <heinhtetlulu14@gmail.com>
2024-01-02 23:15:26 -0800

    Add shadcn form to store modal

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -2,9 +2,39 @@
 
 import useStoreModal from "@/hooks/useStoreModal";
 import Modal from "@/components/ui/modal";
+import * as z from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Required",
+    }),
+})
 
 const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
 
     return (
         <Modal
@@ -13,7 +43,31 @@ const StoreModal = () => {
             isOpen={storeModal.isOpen}
             onClose={storeModal.onClose}
         >
-            Form
+            <div>
+                <div className="pt-2 pb-4 space-y-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="E-commerce" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="flex items-center justify-end">
+                                <Button type="submit">Continue</Button>
+                            </div>
+
+                        </form>
+                    </Form>
+                </div>
+            </div>
         </Modal>
     );
 }
