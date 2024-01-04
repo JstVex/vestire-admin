3a4a3a5cc61fc4768fd8e3b2a7d86bc731b47f35
JstVex <heinhtetlulu14@gmail.com>
2024-01-03 21:32:28 -0800

    Implement form submission for creating a new store

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -15,6 +15,9 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -25,6 +28,8 @@ const formSchema = z.object({
 const StoreModal = () => {
     const storeModal = useStoreModal();
 
+    const [loading, setLoading] = useState(false);
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -32,8 +37,17 @@ const StoreModal = () => {
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setLoading(true);
+
+            const response = await axios.post("/api/stores", values);
+            toast.success("Store created");
+        } catch (error) {
+            toast.error("Something went wrong");
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
@@ -54,14 +68,14 @@ const StoreModal = () => {
                                     <FormItem>
                                         <FormLabel>Name</FormLabel>
                                         <FormControl>
-                                            <Input placeholder="E-commerce" {...field} />
+                                            <Input disabled={loading} placeholder="E-commerce" {...field} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
                                 )}
                             />
                             <div className="flex items-center justify-end">
-                                <Button type="submit">Continue</Button>
+                                <Button disabled={loading} type="submit">Continue</Button>
                             </div>
 
                         </form>
