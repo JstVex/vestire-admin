63950c6bb567bf4e5b54b4693720adb182ea40a3
JstVex <heinhtetlulu14@gmail.com>
2024-01-17 19:08:02 -0800

    Add Overview and Billboards route

--- components/navbar/navItems.tsx
+++ components/navbar/navItems.tsx
@@ -10,6 +10,16 @@ const NavItems = () => {
 
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: 'Overview',
+            active: pathname === `/${params.storeId}`
+        },
+        {
+            href: `/${params.storeId}/billboards`,
+            label: 'Billboards',
+            active: pathname === `/${params.storeId}/billboards`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`
